9904f9cbe2f204dcc483a5b79b0429849b1b7bf6
William <97297182+WilliamSlavchenko@users.noreply.github.com>
2024-07-18 01:40:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useInView } from "react-intersection-observer";
 import Intro from './components/Intro';
 import Body from './components/Body';
 import Square from './components/Square';
@@ -7,13 +8,13 @@ import About1 from './components/About1';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Decos from './components/Decos';
-import RotatingSquares from './components/RotatingSquares';
 
 function App() {
+    const { ref: introRef, inView: introIsVisible } = useInView();   
     return (
         <div className="App h-screen overflow-y-auto">
             <div className="relative flex h-screen">
-                <div className="flex-grow-0 flex-shrink-0 basis-4/6 bg-richgray h-full">
+                <div ref = {introRef} className="flex-grow-0 flex-shrink-0 basis-4/6 bg-richgray h-full">
                     <Intro />
                     <Body />
                     <Decos/>
@@ -26,7 +27,6 @@ function App() {
             <About1 />
             <Projects />
             <Contact />
-            <RotatingSquares /> {/* Include RotatingSquares component */}
         </div>
     );
 }
